bb3f85aa7fb4d2980c8ba40e471bdc09a13d78ab
DanGraziano <dangraziano@outlook.com>
2022-07-02 17:29:36 -0400

    Add grid template styling via DOM

--- script.js
+++ script.js
@@ -37,18 +37,20 @@ sizeBtn.addEventListener("click", getPlayerSelection)
 // function for button click to generate new grids
 
 function getPlayerSelection() {
-    let playerInput = prompt("Choose a number between 10 and 50 to determine the size of the board");
+    let playerInput = prompt("Choose a number between 10 and 65 to determine the size of the board");
     let playerNumber = parseInt(playerInput);
-    while (playerNumber < 10 || playerNumber > 50) {
-        playerInput = prompt("Choose a number between 10 and 50 to determine the size of the board")
+    while (playerNumber < 10 || playerNumber > 65) {
+        playerInput = prompt("Choose a number between 10 and 65 to determine the size of the board")
         playerNumber = parseInt(playerInput);
-        if (playerNumber > 10 || playerNumber < 51) {
+        if (playerNumber > 10 || playerNumber < 66) {
             break
         }    
     }
-    divContainer.style.setProperty("--grid-size", playerNumber)
-    let gridSize = playerNumber * playerNumber
-    for (i = 0; i < gridSize ; i++) {
+    let gridTemplateSize = 'repeat('+playerNumber+', 1fr)'
+    divContainer.style.gridTemplateColumns = gridTemplateSize;
+    divContainer.style.gridTemplateRows = gridTemplateSize;
+    let gridNumber = playerNumber * playerNumber
+    for (i = 0; i < gridNumber ; i++) {
         let newDiv = document.createElement('div');
         newDiv.className = "grid-item";
         newDiv.addEventListener('mouseover', event => {
