5276b0db427599021effa1535014cbb595329707
DanGraziano <dangraziano@outlook.com>
2022-07-02 16:30:36 -0400

    Add function for dynamic grid size and hover trail

--- script.js
+++ script.js
@@ -1,22 +1,46 @@
+// buttons
 
-// Create div container
-
-const divContainer = document.createElement('div');
-    divContainer.className = "grid-container";
-    document.body.appendChild(divContainer);
-    
-// Create 256 div
-
-for (i = 0; i < 256 ; i++) {
-    let newDiv = document.createElement('div');
-    newDiv.className = "grid-item";
-    divContainer.appendChild(newDiv);
+const btnContainer = document.createElement('div');{
+    btnContainer.className = "btn-container";
+    document.body.appendChild(btnContainer);
 }
 
-// Add a button to the top of the screen that will send a user a
-// popup asking for the number of squares per side for the new grid
-// Once entered the existing grid should be removed and a new grid generated
+const sizeBtn = document.createElement("button"); {
+    sizeBtn.className = "size-button";
+    sizeBtn.innerText = "Enter Grid Size";
+    btnContainer.appendChild(sizeBtn);
+}
 
+// create  div container
 
-// CSS needs to change so the grid is changed to the playerInput
+const divContainer = document.createElement('div');{
+    divContainer.className = "grid-container";
+    document.body.appendChild(divContainer);
+}
 
+// button click
+
+sizeBtn.addEventListener("click", getPlayerSelection)
+
+// function for button click to generate new grids
+
+function getPlayerSelection() {
+    let playerInput = prompt("Choose a number between 5 and 50 to set the amount of squares you would like per side");
+    let playerNumber = parseInt(playerInput);
+    while (playerNumber < 2 || playerNumber > 50) {
+        prompt("Choose a number between 2 and 50 to set the amount of squares you would like per side")
+        if (playerNumber > 1 || playerNumber < 51) {
+            break
+        }    
+    }
+    divContainer.style.setProperty("--grid-size", playerNumber)
+    let gridSize = playerNumber * playerNumber
+    for (i = 0; i < gridSize ; i++) {
+        let newDiv = document.createElement('div');
+        newDiv.className = "grid-item";
+        newDiv.addEventListener('mouseover', event => {
+            event.target.classList.add('hover-trail')
+        })
+        divContainer.appendChild(newDiv);
+    }
+}
